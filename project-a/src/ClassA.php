e18a173721e22c104102e76e7837a46ed59b73d2
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2016-09-17 19:17:51 +0200

    Added ClassA and ClassB

---
+++ project-a/src/ClassA.php
@@ -0,0 +1,9 @@
+<?php
+
+class ClassA
+{
+    public function __construct()
+    {
+        printf("A says \"Hello\"\n");
+    }
+}
---
+++ project-b/src/ClassB.php
@@ -0,0 +1,9 @@
+<?php
+
+class ClassB
+{
+    public function __construct()
+    {
+        printf("B says \"Hello\"\n");
+    }
+}
